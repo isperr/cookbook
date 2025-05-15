b4af1d475cec557f25b284db7776c3f20ccfffb6
isperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2025-05-15 17:59:51 +0200

    chore: add mui with theme setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,52 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline'
+import {StyledEngineProvider} from '@mui/material'
+
 import './index.css'
 import App from './App.tsx'
+import AppWithTheme from './AppWithTheme.tsx'
 
-createRoot(document.getElementById('root')!).render(
+import '@fontsource/jost/300.css'
+import '@fontsource/jost/400.css'
+import '@fontsource/jost/500.css'
+import '@fontsource/jost/700.css'
+
+const rootElement = document.getElementById('root')!
+const root = createRoot(rootElement)
+
+root.render(
   <StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <AppWithTheme
+        appThemeOptions={{
+          components: {
+            MuiPopover: {
+              defaultProps: {
+                container: rootElement
+              }
+            },
+            MuiPopper: {
+              defaultProps: {
+                container: rootElement
+              }
+            },
+            MuiDialog: {
+              defaultProps: {
+                container: rootElement
+              }
+            },
+            MuiModal: {
+              defaultProps: {
+                container: rootElement
+              }
+            }
+          }
+        }}
+      >
+        <CssBaseline />
+        <App />
+      </AppWithTheme>
+    </StyledEngineProvider>
   </StrictMode>
 )
---
+++ src/theme.tsx
@@ -0,0 +1,139 @@
+import {ThemeOptions} from '@mui/material/styles'
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#6db07d'
+    },
+    secondary: {
+      main: '#a67c45'
+    },
+    warning: {
+      main: '#f59a23'
+    },
+    info: {
+      main: '#00c7e0'
+    },
+    success: {
+      main: '#7ec636'
+    }
+  },
+  typography: {
+    fontFamily: 'Jost',
+    fontSize: 16
+  },
+  spacing: 8,
+  shape: {
+    borderRadius: 4
+  },
+  components: {
+    MuiSwitch: {
+      defaultProps: {
+        size: 'small'
+      },
+      styleOverrides: {
+        root: {
+          width: 42,
+          height: 26,
+          padding: 0,
+          margin: 8
+        },
+        switchBase: {
+          padding: 1,
+          '&$checked, &$colorPrimary$checked, &$colorSecondary$checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + $track': {
+              opacity: 1,
+              border: 'none'
+            }
+          }
+        },
+        thumb: {
+          width: 24,
+          height: 24
+        },
+        track: {
+          borderRadius: 13,
+          border: '1px solid #bdbdbd',
+          backgroundColor: '#fafafa',
+          opacity: 1,
+          transition:
+            'background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms'
+        }
+      }
+    },
+    MuiList: {
+      defaultProps: {
+        dense: true
+      }
+    },
+    MuiMenuItem: {
+      defaultProps: {
+        dense: true
+      }
+    },
+    MuiTable: {
+      defaultProps: {
+        size: 'small'
+      }
+    },
+    MuiButton: {
+      defaultProps: {
+        size: 'small'
+      }
+    },
+    MuiButtonGroup: {
+      defaultProps: {
+        size: 'small'
+      }
+    },
+    MuiCheckbox: {
+      defaultProps: {
+        size: 'small'
+      }
+    },
+    MuiFab: {
+      defaultProps: {
+        size: 'small'
+      }
+    },
+    MuiFormControl: {
+      defaultProps: {
+        margin: 'dense',
+        size: 'small'
+      }
+    },
+    MuiFormHelperText: {
+      defaultProps: {
+        margin: 'dense'
+      }
+    },
+    MuiIconButton: {
+      defaultProps: {
+        size: 'small'
+      }
+    },
+    MuiInputBase: {
+      defaultProps: {
+        margin: 'dense'
+      }
+    },
+    MuiInputLabel: {
+      defaultProps: {
+        margin: 'dense'
+      }
+    },
+    MuiRadio: {
+      defaultProps: {
+        size: 'small'
+      }
+    },
+    MuiTextField: {
+      defaultProps: {
+        margin: 'dense',
+        size: 'small'
+      }
+    }
+  }
+}
